0721aac0dfcaf72d19a6ba7c5a3281e91fabd8cd
HoldenGjuka <holdengjuka@gmail.com>
2022-10-30 14:36:55 -0700

    Turned off debug, added webserver IP to allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = open(r"./secret-key.txt", "r")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.4.203"]
 
 
 # Application definition
