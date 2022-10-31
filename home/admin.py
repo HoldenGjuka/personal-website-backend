9666327de81d299839e9f36c7c2acaef3b31598f
HoldenGjuka <holdengjuka@gmail.com>
2022-10-30 20:49:27 -0700

    Registered model Resume with admin site

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import BlogPost, Image
+from .models import BlogPost, Image, Resume
 
 admin.site.register(BlogPost)
 admin.site.register(Image)
+admin.site.register(Resume)
