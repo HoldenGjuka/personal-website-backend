86893ff5ecddf459c5c7947c2b0a9ccc5b7f07e0
HoldenGjuka <holdengjuka@gmail.com>
2022-10-30 20:52:02 -0700

    Fixed naming error: model Resume is actually PDF

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from .models import BlogPost, Image, Resume
+from .models import BlogPost, Image, PDF
 
 admin.site.register(BlogPost)
 admin.site.register(Image)
-admin.site.register(Resume)
+admin.site.register(PDF)
