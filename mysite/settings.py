5405ca3fc0cd7c496376957a80159ef0f296c730
holden <holdengjuka@gmail.com>
2022-11-14 20:23:19 -0800

    Allowed host is now set proper on django server settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,7 +16,7 @@ def read_line_from_file(filepath):
 
 SECRET_KEY = read_line_from_file('./etc/secret-key.txt')
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["73.169.221.60"]
 
 # Application definition
 
