29c2706c1af8623c4fc07805b9c03c1c4b06d05f
Holden Gjuka <holdengjuka@gmail.com>
2022-11-16 16:18:46 -0800

    Resolved environment activation bug due to improper file pathing

--- run_dev.sh
+++ run_dev.sh
@@ -1,7 +1,7 @@
 echo "Is debug turned on?"
 
 echo "Activating environment..."
-source ./../bin/activate
+source ../bin/activate
 
 echo "Starting Development Server..."
 python manage.py runserver 127.0.0.1:7000
