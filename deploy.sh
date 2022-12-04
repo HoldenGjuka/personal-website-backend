a2e596008cf7df870308a3483452e08ac330fda8
holden <holdengjuka@gmail.com>
2022-12-03 20:47:45 -0800

    Modified deploy script to reflect new port

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ echo "Activating Python environment..."
 source ../bin/activate
 
 echo "Deploying backend server..."
-python manage.py runserver 127.0.0.1:7000
+python manage.py runserver 127.0.0.1:8000
 
 echo "Server deployed!"
